6cee3ece93245f0a32758e532aa937b9291df3b6
Vasu Sharma <techid2011@outlook.com>
2022-01-24 13:23:21 +0530

    occupation controller and api added

---
+++ PremiumCalculator/Controllers/OccupationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PremiumCalculator.Api.DTO;
+using PremiumCalculator.Core.Occupations;
+
+namespace PremiumCalculator.Api.Controllers
+{
+    /// <summary>
+    /// Controller to manage Occupation 
+    /// </summary>
+    [Route("api/v1/occupations")]
+    [ApiController]
+    public class OccupationController : ControllerBase
+    {
+        private readonly IOccupationQuery _occupationQuery;
+        public OccupationController(IOccupationQuery occupationQuery)
+        {
+            _occupationQuery = occupationQuery;
+        }
+
+        /// <summary>
+        /// Fetches list of Occupations 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Occupations> Get()
+        {
+            List<Occupations> occupations = _occupationQuery.Execute().Select(x => new Occupations { 
+                Id = x.Id,
+                Description = x.Description
+            }).ToList();
+            return occupations;
+        }
+
+    }
+}
---
+++ PremiumCalculator/DTO/Occupations.cs
@@ -0,0 +1,8 @@
+﻿namespace PremiumCalculator.Api.DTO
+{
+    public class Occupations
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
